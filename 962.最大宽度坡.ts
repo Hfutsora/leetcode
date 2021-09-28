dc39a3c03f158aa6c081d56b5e763d9043eae8a8
童锴 <346762712@qq.com>
2021-09-28 15:46:31 +0800

    feat: add leetcode #962 solution

--- 962.最大宽度坡.ts
+++ 962.最大宽度坡.ts
@@ -5,22 +5,49 @@
  */
 
 // @lc code=start
+// function maxWidthRamp(nums: number[]): number {
+//   let max = 0;
+//   const lastIndex = nums.length - 1;
+
+//   for(let i = 0; i < lastIndex; i++) {
+//     if(i >= lastIndex - max) {
+//       return max;
+//     }
+
+//     for(let j = lastIndex; j > i; j--) {
+//       if(j - i <= max) {
+//         continue;
+//       }
+
+//       if(nums[j] >=  nums[i]) {
+//         if(j - i > max) {
+//           max = j - i;
+//         }
+
+//         continue;
+//       }
+//     }
+//   }
+
+//   return max;
+// };
+
+
 function maxWidthRamp(nums: number[]): number {
   let max = 0;
-  const lastIndex = nums.length - 1;
+  const decreasArray: number[] = [];
 
-  for(let i = 0; i < lastIndex; i++) {
-    if(i >= lastIndex - max) {
-      return max;
+  for(let i = 0; i < nums.length; i++) {
+    if(!decreasArray.length || nums[i] < nums[decreasArray[decreasArray.length - 1]]) {
+      decreasArray.push(i);
     }
+  }
 
-    for(let j = lastIndex; j > i; j--) {
-      if(j - i <= max) {
-        continue;
-      }
-
-      if(nums[j] >=  nums[i] && j - i > max) {
-        max = j - i;
+  for(let i = nums.length - 1; i > 0; i--) {
+    while(decreasArray.length && nums[i] >= nums[decreasArray[decreasArray.length - 1]]) {
+      const index = decreasArray.pop();
+      if(i - index > max) {
+        max = i - index;
       }
     }
   }
