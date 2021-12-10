ab1ee352245decf6ba25f146522b377272b1533b
童锴 <346762712@qq.com>
2021-12-10 13:50:02 +0800

    perf(leetcode): update leetcode #1679 solution

--- 1679.k-和数对的最大数目.ts
+++ 1679.k-和数对的最大数目.ts
@@ -4,8 +4,6 @@
  * [1679] K 和数对的最大数目
  */
 
-// @lc code=start
-
 // function maxOperations(nums: number[], k: number): number {
 //   let answer = 0;
 
@@ -50,49 +48,74 @@
 //   return answer;
 // };
 
+// function maxOperations(nums: number[], k: number): number {
+//   let answer = 0;
+
+//   const numMap = new Map();
+//   const numSet = Array.from(new Set(nums));
+
+//   for(const num of nums) {
+//     const getNum = numMap.get(num) || 0;
+
+//     numMap.set(num, getNum + 1);
+//   }
+
+//   while(numMap.size >= 1) {
+//     let find = false;
+
+//     for(let i = 1; i < numSet.length; i++) {
+//       if(numSet[0] + numSet[i] === k) {
+//         find = true; 
+//         answer += Math.min(numMap.get(numSet[0]), numMap.get(numSet[i]));
+
+//         numMap.delete(numSet[0]);
+//         numMap.delete(numSet[i]);
+
+//         numSet.splice(0 ,1);
+//         numSet.splice(i - 1 ,1);
+
+//         break;
+//       }
+//     }
+
+//     if(!find) {
+//       const current = numSet[0];
+
+//       if(current + current === k) {
+//         answer += Math.floor(numMap.get(current) / 2);
+//       }
+
+//       numMap.delete(numSet[0]);
+//       numSet.splice(0 ,1);
+//     }
+//   }
+
+//   return answer;
+// };
+
+// @lc code=start
 function maxOperations(nums: number[], k: number): number {
   let answer = 0;
+  const sort = nums.sort((a ,b) => a - b);
 
-  const numMap = new Map();
-  const numSet = Array.from(new Set(nums));
+  let i = 0;
+  let j = nums.length - 1;
 
-  for(const num of nums) {
-    const getNum = numMap.get(num) || 0;
+  while(i < j) {
+    const sum = sort[i] + sort[j];
 
-    numMap.set(num, getNum + 1);
-  }
-
-  while(numMap.size >= 1) {
-    let find = false;
-
-    for(let i = 1; i < numSet.length; i++) {
-      if(numSet[0] + numSet[i] === k) {
-        find = true; 
-        answer += Math.min(numMap.get(numSet[0]), numMap.get(numSet[i]));
-
-        numMap.delete(numSet[0]);
-        numMap.delete(numSet[i]);
-
-        numSet.splice(0 ,1);
-        numSet.splice(i - 1 ,1);
-
-        break;
-      }
-    }
-
-    if(!find) {
-      const current = numSet[0];
-
-      if(current + current === k) {
-        answer += Math.floor(numMap.get(current) / 2);
-      }
-
-      numMap.delete(numSet[0]);
-      numSet.splice(0 ,1);
+    if(sum === k) {
+      answer += 1;
+      i += 1;
+      j -= 1;
+    } else if (sum > k) {
+      j -= 1;
+    } else {
+      i += 1;
     }
   }
 
   return answer;
-};
+}
 // @lc code=end
 
