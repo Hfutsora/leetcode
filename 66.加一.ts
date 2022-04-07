4f1d9453eb3577f739c482c98cc95f6d23ebb13e
hfutsora <346762712@qq.com>
2022-04-07 22:08:51 +0800

    leetcode #66

---
+++ 66.加一.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=typescript
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+function plusOne(digits: number[]): number[] {
+  const result: number[] = [];
+  let plus: boolean;
+
+  const tail = digits.pop();
+
+  if(tail === 9) {
+    plus = true;
+    result.unshift(0);
+  } else {
+    result.unshift(tail + 1);
+  }
+
+  while(digits.length) {
+    const tail = digits.pop();
+
+    if(plus) {
+      if(tail === 9) {
+        result.unshift(0);
+      } else {
+        plus = false;
+        result.unshift(tail + 1);
+      }
+    } else {
+      result.unshift(tail);
+    }
+  }
+
+  if(plus) result.unshift(1);
+
+  return result;
+};
+// @lc code=end
+
