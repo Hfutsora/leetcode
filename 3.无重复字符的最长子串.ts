e28ba5d0623dca86b395998ebaf51438ff41aea1
童锴 <346762712@qq.com>
2022-04-20 16:39:58 +0800

    pref: leetcode #3

--- 3.无重复字符的最长子串.ts
+++ 3.无重复字符的最长子串.ts
@@ -6,39 +6,19 @@
 
 // @lc code=start
 function lengthOfLongestSubstring(s: string): number {
-  if(s.length < 2) return s.length;
-
   let max = 0
+  const nums: string[] = []
 
-  for(let i = 0; i < s.length - 1; i++) {
-    for(let j = i + 1; j < s.length; j++) {
-      const substring = s.substring(i, j + 1);
-      let isNotRAtLast = false;
-
-      if(isNotR(isNotRAtLast, substring)) {
-        max = Math.max(max, substring.length);
-        isNotRAtLast = true;
-      } else {
-        break;
-      }
-    }
-  }
-
-  function isNotR(isNotRAtLast: boolean, s: string) {
-    if(isNotRAtLast) return s[s.length - 1] !== s[0];
-
-    const set = new Set<string>();
-
-    for(let char of s) {
-      if(set.has(char)) return false
-
-      set.add(char)
+  for(let char of s) {
+    while(nums.includes(char)) {
+      nums.shift()
     }
 
-    return true;
+    nums.push(char)
+    max = Math.max(max, nums.length)
   }
-
-  return max || 1;
+  
+  return max;
 };
 // @lc code=end
 
