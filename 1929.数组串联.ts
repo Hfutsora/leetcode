46049b0f96c8293e3348bfc6fd843650fa723611
hfutsora <346762712@qq.com>
2022-07-07 21:10:55 +0800

    feat: leetcode #1929

---
+++ 1929.数组串联.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=1929 lang=typescript
+ *
+ * [1929] 数组串联
+ */
+
+// @lc code=start
+function getConcatenation(nums: number[]): number[] {
+  return [...nums, ...nums];
+};
+// @lc code=end
+
